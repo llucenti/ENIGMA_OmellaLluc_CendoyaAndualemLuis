7fc01b905eb0682098929f04c4405bffb1766b7c
llucenti <lluc.omella@enti.cat>
2026-02-11 17:46:33 +0100

    Update PreXifrat.h
    He afegit la funcio per definir les posicions inicials a la capçalera

--- ENIGMA_OmellaLluc_CendoyaAndualemLuis/ENIGMA_OmellaLluc_CendoyaAndualemLuis/PreXifrat.h
+++ ENIGMA_OmellaLluc_CendoyaAndualemLuis/ENIGMA_OmellaLluc_CendoyaAndualemLuis/PreXifrat.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
 
-//Inicialització funcions
+//InicialitzaciÃ³ funcions
 void asignacioRotors(std::string& rotor1, std::string& rotor2, std::string& rotor3, std::string& notchRot1, std::string& notchRot2, std::string& notchRot3);
 void editarRotors(std::string rotor1, std::string rotor2, std::string rotor3, std::string notchRotor1, std::string notchRotor2, std::string notchRotor3);
 void menuPrincipal(short opcio, std::string missatge, std::string rotor1, std::string rotor2, std::string rotor3, std::string notchRot1, std::string notchRot2, std::string notchRot3, bool notchRot1Found, bool notchRot2Found, std::string mensajeEncriptado);
+void definirPosicionsInicials(std::string& rot1, std::string& rot2, std::string& rot3);
